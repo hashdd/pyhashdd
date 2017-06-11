a61052f5ad3e374e572d8a88950ea5936ea30cb0
brad-anton <brad.antonie@gmail.com>
2017-06-11 16:38:05 -0400

    better module include

--- setup.py
+++ setup.py
@@ -22,15 +22,17 @@ limitations under the License.
 
 
 
-from __future__ import unicode_literals
+# from __future__ import unicode_literals
 
 from codecs import open   # pylint:disable=redefined-builtin
-from os.path import dirname, join
+from os.path import dirname, join, exists, basename, isfile
+from os import listdir
 import sys
 import re
+import platform
 
 from setuptools import setup, find_packages
-
+from shutil import copyfile
 
 CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable',
@@ -47,10 +49,34 @@ CLASSIFIERS = [
     'Topic :: Software Development :: Libraries :: Python Modules',
 ]
 
+def get_libs(t):
+    """
+    t -- type either 'algorithms' or 'features'
+    """
+    p = join('libs', '{}'.format(platform.system().lower()), 'x86_{}'.format(platform.architecture()[0][:2]), '{}'.format(t))
+    if exists(p):
+        return [join(p, f) for f in listdir(p) if isfile(join(p, f))]
+    return []
+
+
+def copy_libs():
+    package_data = []
+    for t in ['algorithms', 'features']:
+        libs = get_libs(t)
+        for l in libs:
+            dst = join('hashdd', '{}'.format(t), basename(l))
+            print 'Prep: Copying {} to {}'.format(l, dst)
+            copyfile(l, dst)
+            package_data.append(join('{}'.format(t), basename(l)))
+
+    print package_data
+    return package_data
+
+
 def main():
     base_dir = dirname(__file__)
     install_requires = open('requirements.txt').read().splitlines()
-    with open('hashdd/version.py', 'r', encoding='utf-8') as config_py:
+    with open('hashdd/version', 'r', encoding='utf-8') as config_py:
         version = re.search(r'^\s+__version__\s*=\s*[\'"]([^\'"]*)[\'"]', config_py.read(), re.MULTILINE).group(1)
 
     setup(
@@ -60,7 +86,9 @@ def main():
         long_description=open(join(base_dir, 'README.md'), encoding='utf-8').read(),
         author='hashdd',
         url='https://www.hashdd.com',
-        packages=find_packages(exclude=['bin', 'docs']),
+        packages=find_packages(exclude=['bin', 'docs', 'libs']),
+        package_data={ 'hashdd': copy_libs() },
+        #zip_safe=False,
         scripts=['bin/hashdd'],
         install_requires=install_requires,
         classifiers=CLASSIFIERS,
